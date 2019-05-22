81f85898601a37382a06c895ef14f741c8bc3180
abdelhaq <abdelhaq.elhassnaoui@gmail.com>
2019-05-22 06:13:09 +0200

    update 2019-05-22

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   //title = 'Accueil';
+  private menu : Boolean;
+  private toggle : Boolean;
+
+  ngOnInit() {
+    this.menu = true;
+    this.toggle = false;
+  }
+
+  public hideMenu(){
+    this.menu = true;
+    this.toggle = false;
+  }
+
+  public displayMenu(){
+    this.menu = false;
+    this.toggle = true;
+  }
 }
